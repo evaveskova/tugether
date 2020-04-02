4619dd49147545c45f9a41fe30bac15834dc3c07
hugopassos <hugo.passos@hotmail.com>
2020-04-02 16:32:34 -0300

    Created authentication features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
   root to: 'static#home'
+  get 'registrations/create'
+  get 'sessions/create'
+  get :logged_in, to: 'sessions#logged_in'
+  delete :logout, to: 'sessions#logout'
 
   resources :sessions, only: :create
   resources :registration, only: :create
